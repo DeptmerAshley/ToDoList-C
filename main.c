b7adfed4aec28aa807f4f5b26a74efb5360c3818
DeptmerAshley <ashledep@oregonstate.edu>
2025-03-31 14:59:31 -0700

    Reworked parse into LinkedList and changed file type from .txt to .csv

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 
 #define MAX_TASKS 100
 
+struct Task *head = NULL;
+
 int mainMenu(int menuChoice)
 {
     printf("\n(1): List Tasks\n(2): Add Task\n(3): Mark Task Completed\n(4): Delete Task\n(5): Save Task to File\n(6): Load Tasks from File\n(0): Exit\n");
@@ -26,25 +28,60 @@ void deleteTask(Task *tasks, int *count, int id);
 
 void saveTasks(Task *tasks, int count);
 
-Task* loadTasks(Task* *userTaskList)  // (Task *tasks, int *count)
-{
-    FILE* file = fopen("tasks.txt", "r");
-    if (file == NULL)
-    {
-        printf("Cannot locate user information\nExiting...\n");
-        return 0;
+void loadTasks(const char* fileName) {
+    FILE *tasks = fopen(fileName, "r");
+    if (!tasks) {
+        printf("Error: User Info Not Found\n");
+        return;
     }
-    //userTaskList = malloc(MAX_TASKS * sizeof(Task));
+    
+    struct Task *current = NULL;
+    char line[250];
+
+    // Skip first line with labels
+    if (fgets(line, sizeof(line), tasks) == NULL) {
+        fclose(tasks);
+        return;
+    }
+
     int numTasks = 0;
-    fscanf(file, "%d\n", &numTasks);
+
+    //Iterate through each line of file and populate the Tasks Linked List
+    while (fgets(line, sizeof(line),  tasks) != NULL) {
+        numTasks++;
+        struct Task *taskObj = malloc(sizeof(struct Task));
+        char *token = strtok(line, ",");
+        if (token) {
+            taskObj->id = atoi(token);
+            token = strtok(NULL, ",");
+
+            if (token) {
+                strcpy(taskObj->description, token);
+                token = strtok(NULL, ",");
+            }
+
+            if (token) {
+                taskObj->completed = atoi(token);
+                token = strtok(NULL, ",");
+                
+                if (head == NULL) {
+                    head = taskObj;
+                    current = taskObj;
+                }
+                else {
+                    current->next = taskObj;
+                    current = taskObj;
+                }
+            }
+        }
+    }
+    printf("\nUser Info loaded...");
 }
 
 
-int main(){
+int main() {
+    loadTasks("tasks.csv");
     
-    Task* userTaskList = NULL;
-    userTaskList = loadTasks(&userTaskList);
-
     int menuChoice;
     while (menuChoice != 0)
     {
--- task.h
+++ task.h
@@ -1,10 +1,12 @@
 #ifndef TASK_H
 #define TASK_H
 
-typedef struct {
+typedef struct Task {
     int id;
     char description[256];
     int completed; // 0 = incomplete, 1 = complete
+
+    struct Task *next;
 } Task;
 
 #endif
